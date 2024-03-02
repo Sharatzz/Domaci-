7706aa8e2330bd461ca70a49370e91fcd331e080
Radovan Sarac <98130853+Sharatzz@users.noreply.github.com>
2024-03-02 17:47:30 +0000

    Test test

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Radovan')
+#Zadatak 1:
+
+def desifrovanje_koda():
+
+    s= 0
+    p = 1
+
+    tb = input('Unesite trocifreni broj')
+
+    
+
+    for _ in tb:
+
+        s = s + int(_)
+        p = p * int(_)
+
+    print(p - s)
+
+
+desifrovanje_koda()
